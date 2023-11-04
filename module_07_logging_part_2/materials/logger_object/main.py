4c1512c150c2e758c558e6952f016c46be2a74d7
Druchek MA <mdruchek@yandex.ru>
2023-11-04 09:26:18 +0300

    homework for video 7.1

--- module_07_logging_part_2/materials/logger_object/main.py
+++ module_07_logging_part_2/materials/logger_object/main.py
@@ -5,19 +5,26 @@ from http_utils import get_ip_address
 from subprocess_utils import get_kernel_version
 
 
-logging.basicConfig(level='DEBUG')
-logger = logging.getLogger('main')
+logger_main = logging.getLogger('main')
+logger_utils = logging.getLogger('utils')
+logger_main.setLevel('INFO')
+logger_utils.setLevel('DEBUG')
+
 
 app = flask.Flask(__name__)
 
 
 @app.route('/get_system_info')
 def get_system_info():
-    logger.info('Start working')
+    logger_main.info('Start working')
     ip = get_ip_address()
     kernel = get_kernel_version()
     return "<p>{}</p><p>{}</p>".format(ip, kernel)
 
 
 if __name__ == '__main__':
+    print(logger_main.parent)
+    print(logger_utils.parent)
+    print(logger_main)
+    print(logger_utils)
     app.run(debug=True)
