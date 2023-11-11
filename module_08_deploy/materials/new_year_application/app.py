73cca66be8edc3c728ec32f29e7704e2be897529
Druchek MA <mdruchek@yandex.ru>
2023-11-11 08:53:32 +0300

    added static folder

--- module_08_deploy/materials/new_year_application/app.py
+++ module_08_deploy/materials/new_year_application/app.py
@@ -1,14 +1,19 @@
 import os
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, url_for
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
 template_folder = os.path.join(root_dir, "templates")
-js_directory = os.path.join(template_folder, "js")
-css_directory = os.path.join(template_folder, "css")
-images_directory = os.path.join(template_folder, "images")
-
+static_directory = os.path.join(template_folder, "static")
+js_directory = os.path.join(static_directory, "js")
+css_directory = os.path.join(static_directory, "css")
+images_directory = os.path.join(static_directory, "images")
+static_directories = {
+    'js': js_directory,
+    'css': css_directory,
+    'images': images_directory
+}
 app = Flask(__name__, template_folder=template_folder)
 
 
@@ -17,19 +22,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/js/<path:path>")
-def send_js(path):
-    return send_from_directory(js_directory, path)
-
-
-@app.route("/css/<path:path>")
-def send_css(path):
-    return send_from_directory(css_directory, path)
-
-
-@app.route("/images/<path:path>")
-def send_images(path):
-    return send_from_directory(images_directory, path)
+@app.route("/<path:directory>/<path:path>")
+def send_static(directory, path):
+    return send_from_directory(directory=static_directories[directory], path=path)
 
 
 if __name__ == "__main__":
