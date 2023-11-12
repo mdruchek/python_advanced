10ae0b372e63bdb3bc1669eb66eeaabc3e177ff9
Дручек Михаил <druchek_ma@vsw.ru>
2023-11-12 21:43:10 +0300

    module9 is solved

--- module_09_docker/materials/test_application/app.py
+++ module_09_docker/materials/test_application/app.py
@@ -7,13 +7,14 @@ APP = Flask(__name__)
 HOST = '0.0.0.0'
 PORT = 5000
 SERVICE_NAME = os.environ.get('SERVICE_NAME', 'application')
+ADMIN_NAME = os.environ.get('ADMIN_NAME', 'admin')
 
 
 @APP.route('/hello/<user>')
 def hello_user(user: str):
     return make_response(
         jsonify(
-            {'message': f'Hello from {SERVICE_NAME}, {user}!'}
+            {'message': f'Hello from {SERVICE_NAME}, {user}! Admin: {ADMIN_NAME}'}
         ),
         200
     )
